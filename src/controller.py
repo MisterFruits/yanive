6e5c5929c742f46baf32ebf4545d43da7acb28cf
Victor <vic.toad.tor@free.fr>
2014-02-16 15:50:14 +0100

    NEW controller.py base class

---
+++ src/controller.py
@@ -0,0 +1,13 @@
+class GameController(object):
+    """docstring for GameController"""
+    def __init__(self, arg):
+        super(GameController, self).__init__()
+        self.arg = arg
+
+
+class PlayerController(object):
+    """docstring for PlayerController"""
+    def __init__(self, arg):
+        super(PlayerController, self).__init__()
+        self.arg = arg
+
